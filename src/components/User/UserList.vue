75df2651d8c2df675ec632e06e9d9f5909d14c03
NadetDev <nadetdev@gmail.com>
2025-05-29 14:19:21 +0200

    add loader

--- src/components/User/UserList.vue
+++ src/components/User/UserList.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container p-4">
+  <div class="d-flex justify-content-center p-5 m-5" v-if="userList.isLoading">
+    <span class="loader"></span>
+  </div>
+  <div class="container p-4 m-3 bg-white" v-if="!userList.isLoading">
     <div v-if="userList.users.length > 0">
       <h1 class="text-success text-center">User List</h1>
       <table class="table table-striped table-light">
@@ -19,7 +22,9 @@
         </tbody>
       </table>
     </div>
-    <div v-else class="alert alert-warning text-center" role="alert">No user found</div>
+    <div v-if="userList.users.length === 0" class="alert alert-warning text-center" role="alert">
+      No user found
+    </div>
   </div>
 </template>
 <script setup>
@@ -31,17 +36,57 @@ const apiLocal = 'http://localhost:3001/users'
 
 const userList = reactive({
   users: [],
+  isLoading: false,
 })
 
-onMounted(async () => {
+onMounted(() => {
+  loadProducts()
+})
+
+const loadProducts = () => {
+  userList.isLoading = true
   axios.get(apiLocal).then((response) => {
-    userList.users = response.data
+    new Promise((resolve) => setTimeout(resolve, 2000)).then(() => {
+      userList.users = response.data
+      userList.isLoading = false
+    })
   })
-
-  /* await fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      userList.users = data
-    }) */
-})
+}
 </script>
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  position: relative;
+  transform: rotate(45deg);
+  background: #fff;
+}
+.loader::before {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  inset: 0px;
+  border-radius: 50%;
+  border: 24px solid #ff3d00;
+  animation: prixClipFix 2s infinite linear;
+}
+
+@keyframes prixClipFix {
+  0% {
+    clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+  }
+  25% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+  }
+  50% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+  }
+  75% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+  }
+  100% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+  }
+}
+</style>
