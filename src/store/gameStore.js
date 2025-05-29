47504602dedbb89e596cc392620b213f751942ce
NadetDev <nadetdev@gmail.com>
2025-05-29 16:49:54 +0200

    store in composition api style

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -1,34 +1,45 @@
+import { computed, ref } from 'vue'
+
 import { defineStore } from 'pinia'
 
-export const useGameStore = defineStore('gameStore', {
-  state: () => ({
-    score: 50,
-    maxHeath: 100,
-    maxAttack: 30,
-    maxDefense: 10,
-  }),
+export const useGameStore = defineStore('gameStore', () => {
+  const score = ref(50)
+  const maxHeath = ref(100)
+  const maxAttack = ref(30)
+  const maxDefense = ref(10)
 
-  getters: {
-    getWiningScore: (state) => state.maxHeath,
-    getAttack: (state) => state.maxAttack,
-    getDefense: (state) => state.maxDefense,
-    getScore: (state) => state.score,
-  },
-  actions: {
-    setNextAttack() {
-      console.log('get next attack')
-      let attack = Math.floor(Math.random() * this.maxAttack) + 1
-      console.log('attack is: ', attack)
-      return (this.score += attack)
-    },
-    setNextDefense() {
-      console.log('get next defense')
-      let defense = Math.floor(Math.random() * this.maxDefense) + 1
-      console.log('defense is: ', defense)
-      return (this.score -= defense)
-    },
-    resetScore() {
-      this.score = 50
-    },
-  },
+  const getWiningScore = computed(() => maxHeath.value)
+  const getAttack = computed(() => maxAttack.value)
+  const getDefense = computed(() => maxDefense.value)
+  const getScore = computed(() => score.value)
+
+  const setNextAttack = () => {
+    console.log('get next attack')
+    let attack = Math.floor(Math.random() * this.maxAttack) + 1
+    console.log('attack is: ', attack)
+    return (this.score += attack)
+  }
+  const setNextDefense = () => {
+    console.log('get next defense')
+    let defense = Math.floor(Math.random() * this.maxDefense) + 1
+    console.log('defense is: ', defense)
+    return (this.score -= defense)
+  }
+  const resetScore = () => {
+    this.score = 50
+  }
+
+  return {
+    score,
+    maxHeath,
+    maxAttack,
+    maxDefense,
+    getWiningScore,
+    getAttack,
+    getDefense,
+    getScore,
+    setNextAttack,
+    setNextDefense,
+    resetScore,
+  }
 })
